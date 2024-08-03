305f4fc456e7f562432ec86a9a21f35d84babdd8
Psychology Simango <95903000+Void-Muerte@users.noreply.github.com>
2024-08-03 12:37:14 +0200

    added mutation and context

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {ApolloServer,gql} = require('apollo-server');
 const Users = require('./users');
+const Todos = require("./todos");
+const {randomUUID} = require("crypto");
 
 const typeDefs = gql`
 type Query{
@@ -7,11 +9,25 @@ type Query{
     users:[User],
     user(id:ID!):User
 }
+input InputUser{
+    firstName:String!
+    lastName:String!
+    email:String!
+    password:String!
+}
+type Todo{
+    title:String!
+    by:ID!
+}
 type User{
     id:ID!
     firstName:String!
     lastName:String!
     email:String!
+    todos:[Todo]
+}
+type Mutation{
+    createUser(newUser:InputUser!):User
 }
 `
 
@@ -21,15 +37,29 @@ const resolvers = {
             return 'hello world'
         },
         users:()=>Users,
-        user:(parent,{id}, context)=>{
-            console.log(id);
+        user:(parent,{id}, {userLoggedIn})=>{
+            if(!userLoggedIn){
+                throw new Error("User needs to log in!");
+            }
             return Users.find(item=>item.id==id)
+        }
+    },
+    User:{
+        todos:(parent)=>{
+            return Todos.filter(todo=>todo.by==parent.id);
+        }
+    },
+    Mutation:{
+        createUser:(_, {newUser})=>{
+            const createdUser = {id:randomUUID(), ...newUser};
+            Users.push({...createdUser});
+            return createdUser;
         }
     }
 }
 
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({typeDefs, resolvers,context:{userLoggedIn:true}});
 
 
 // The listen method lauches a web server
---
+++ todos.js
@@ -0,0 +1,6 @@
+const todos = [
+    {title:"A walk through GraphQl", by:"1"},
+    {title:"GraphQL for dummies", by:"3"},
+    {title:"GraphQL simplified!", by:"1"}
+]
+module.exports = todos;
